4039cc85dece1d450db05e200d57a4ffb8cc454c
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-07 01:46:06 -0400

    Updated app controller, added regisrtration controller, users controller.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationController < ApplicationController
+	def create
+		@user = User.new(username: params["username"],
+						email: params["email"],
+						passward: params["password"])
+		@user.ensure_auth_token
+		if @user.save
+			render json: { user: @user.as_json(only: :username, :email, :auth_token )}
+				status:  :created
+		else
+			render json: { errors: @user.errors.full_messages },
+					status: :unprocessable_entity
+		end
+	end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+	before_action :authenticate!, only: [:check_auth]
+
+	def login
+		@user = User.find_by!(email: params["email"])
+		if @user.authenticate(params["password"])
+			render json: { @user.as_json(only: [:email, :access_token]) },
+					status: :ok
+		else
+			render json: { message: "INVALID EMAIL OR PASSWORD."}
+					status: :unauthorized
+		end
+	end
+
+	def check_auth
+		if current_user
+			render json: { message: "CURRENT USER: #{current_user.email}"},
+					status: :ok
+		else
+			render json: { message: "AUTHORIZATION IS DOWN."},
+					status: :unauthorized
+		end
+	end
+end
