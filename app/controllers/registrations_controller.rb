3c9ad5d3ab9446bfc009944ec4fd1e4e28970713
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-07 14:26:58 -0400

    added missing [ ] brackets

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
-class RegistrationController < ApplicationController
+class RegistrationsController < ApplicationController
 	def create
 		@user = User.new(username: params["username"],
 						email: params["email"],
-						passward: params["password"])
+						password: params["password"])
 		@user.ensure_auth_token
 		if @user.save
-			render json: { user: @user.as_json(only: :username, :email, :auth_token )}
-				status:  :created
+			render json: { user: @user.as_json(only: [:username, :email, :auth_token] )},
+				status: :created
 		else
 			render json: { errors: @user.errors.full_messages },
 					status: :unprocessable_entity
