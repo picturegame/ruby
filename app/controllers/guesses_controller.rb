2943e344645ef89c008a4f83d7008aa723824f8c
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-10 13:17:01 -0400

    added scoring functionality to guesses controller methods

---
+++ app/controllers/guesses_controller.rb
@@ -0,0 +1,25 @@
+class GuessesController < ApplicationController
+	before_action :authenticate!, only [:new]
+
+	def new
+		@image = Post.find(params["id"])
+		correct = @image.solution == params[:guess]
+		if correct
+			@image.update(solved: true)
+			current_user.update(points: += @image.points)
+		else
+			@image.update(points: += 1)
+		end
+      	@guess = current_user.guesses.create!(correct: correct,
+                                             post_id: @image.id,
+                                             guess: params["guess"])
+      	render "new.json.jbuilder", status: :created
+      	#readme - if the image id doesnt exist this sends 404 status
+	end
+
+	def show
+		@guess = current_user.guesses.find(params["id"])
+		render "show.json.builder", status: :ok
+		#if fails, will throw a ActiveRecord::Record not found exception
+	end
+end
